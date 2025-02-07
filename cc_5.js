2a08283ba674fc5c1dd31115e4018e0b5f77f82d
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-07 03:21:46 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -7,3 +7,16 @@ let customer = {
 console.log(`Customer Name: ${customer.name}`);
 console.log(`Customer Age: ${customer.age}`);
 console.log(`Customer Email: ${customer.email}`); // Logging each property to console.
+
+// Task 2 Object Methods
+let order = {
+    orderID: 12345,
+    totalAmount: 150,
+    status: "Processing", // Declaring object with properties.
+    displayOrder() {
+        console.log("Order ID: ", this.orderID);
+        console.log("Total Amount: ", this.totalAmount);
+        console.log("Status: ", this.status)
+    }
+}; // Adding method inside object to log order details.
+order.displayOrder(); // Calling method and logging details.
